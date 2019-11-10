180ece3feec2894a003ea1c861a16c45f8696c67
Aldoarevalo <57592844+Aldoarevalo@users.noreply.github.com>
2019-11-10 17:29:57 -0300

    Inicio.php
    Menu de acceso al sistema php

---
+++ Inicio.php
@@ -0,0 +1,173 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Inicio</title>
+        
+    
+        
+        
+        
+        
+  <link rel="stylesheet" href="bootstrap.min.css" />
+ 
+        <link rel="stylesheet" href="estilo.css">
+        
+        
+        	
+   <style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin:auto;
+				width:800px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav {
+				width:1000px; /*Le establecemos un ancho*/
+				margin:0 auto; /*Centramos automaticamente*/
+			}
+
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:black;
+				color: #fcfcfc;
+/*                                font-weight:  bold;*/
+				text-decoration:none;
+                                text-decoration-color: black;
+				padding:10px 26px;
+				display:block;                            
+                                font-size:16px;
+                            
+			}
+			
+			.nav li a:hover {
+				background-color:aquamarine;
+                                text-decoration: none;
+                                  background: dodgerblue;
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+			
+		</style>
+    </head>
+
+   <body id="hhmenu"> 
+       
+   
+       
+   
+    </head>
+    
+    <div id="menu-wrapper">
+    
+    <div id="header">
+			<nav> <!-- Aqui estamos iniciando la nueva etiqueta nav -->
+				<ul class="nav">
+                                    
+					<li><a href="RegistrarLibro.php">Registrar Libro</a>
+                            
+                                        </li>
+                                        <li><a href="RegistrarLector.php">Registrar Lector</a>
+                                        
+                                        </li>
+					<li><a href="RegistrarPrestamo.php">Registrar Prestamo</a>
+						 
+					</li>
+					<li><a href="RegistrarDevolucion.php">Registrar Devolucion</a>
+						 
+					</li>
+                                      
+					<li><a href="">Informes de Prestamos</a>
+                                                        <ul>
+									<li><a href="RegistrarDevolucion.php">Consultar Prestamos</a></li>
+									<li><a href="Consultadevoluciones.php">Consultar Devoluciones</a></li>
+									
+									
+								</ul>
+                                                        </li>		
+						</ul>
+                                        </li> 
+					
+                                        
+				</ul>
+                            
+			</nav><!-- Aqui estamos cerrando la nueva etiqueta nav -->
+		</div>
+    </div>
+   
+        
+   
+          <div id="menu">
+	
+		
+		
+	
+	
+</div>
+    
+     
+
+            
+         
+  
+        
+ 
+     
+            
+        
+         
+             
+       
+        
+      
+     
+                  
+
+ 
+ 
+      
+            
+            
+      </div>
+<li>		<a  href="javascript:Clickheretoprint()" style="font-size:20px;"><button  class="btn btn-success btn-large"><i class="icon-print"></i> Imprimir</button></a></li>
+
+
+
+       
+
+
+   
+
+      
